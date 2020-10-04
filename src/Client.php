42e6100daf6f721c81fac449720eec4eb9b13da8
developergeorg <bg@moderntoken.com>
2020-10-04 15:51:22 +0300

    fixes

--- src/Client.php
+++ src/Client.php
@@ -26,15 +26,25 @@ class Client extends SoapClient
      *
      * @return mixed
      */
-    public function GetCursOnDateXML(DateTime $date)
+    public function getCursOnDateXML(DateTime $date): array
     {
-        $xml = null;
+        $return = [];
         $dateString = $date->format('Y-m-d');
         $result = parent::GetCursOnDateXML(['On_date' => $dateString]);
         /** if any rate exists */
         if ($result->GetCursOnDateXMLResult->any) {
             $xml = new SimpleXMLElement($result->GetCursOnDateXMLResult->any);
+            foreach ($xml->ValuteCursOnDate as $currency) {
+                $return[] = [
+                    'Vname' => $currency->Vname,
+                    'Vnom' => $currency->Vnom,
+                    'Vcurs' => $currency->Vcurs,
+                    'Vcode' => $currency->Vcode,
+                    'VchCode' => $currency->VchCode
+                ];
+            }
         }
-        return $xml;
+
+        return $return;
     }
 }
